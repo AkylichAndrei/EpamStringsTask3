ba5dace90df37625ff0485f4a49b6b99e95b17c8
Akulich Andrei <103625749+AkylichAndrei@users.noreply.github.com>
2022-11-12 11:17:59 +0300

    alignment
    alignment

--- src/strngsTasksOne/TaskFive.java
+++ src/strngsTasksOne/TaskFive.java
@@ -1,34 +1,25 @@
 package strngsTasksOne;
-
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class TaskFive {
 	String str;
 	char[] chArray;
 	int j;
 	void convert(){
-			//преобразовываем строку в массив
-			 chArray = str.toCharArray();
-			//перебираем все элементы массива
-			for( j = 0; j < chArray.length - 1 ; j++){
-			        //находим слово word
-				if(chArray[j] == ' ' && chArray[j + 1] == ' ' ){
-					//сдвигаем массив 
-					shift();
-					//уменьшаем длинну массива
-					chArray = Arrays.copyOf(chArray,chArray.length - 1);
-					j--;
-			        }	
-			}
-			j=chArray.length - 1;
-			while(chArray[j]==' ') {
-				//уменьшаем длинну массива
+		chArray = str.toCharArray();
+		for( j = 0; j < chArray.length - 1 ; j++){
+			if(chArray[j] == ' ' && chArray[j + 1] == ' ' ){
+				shift();
 				chArray = Arrays.copyOf(chArray,chArray.length - 1);
 				j--;
-			}
-			//запихиваем в строку
-			str = String.valueOf(chArray);
+			 }	
+		}
+		j=chArray.length - 1;
+		while(chArray[j]==' ') {
+			chArray = Arrays.copyOf(chArray,chArray.length - 1);
+			j--;
+		}
+		str = String.valueOf(chArray);
 	}
 	void shift() {
 		for(int j2 = j + 1; j2 < chArray.length - 1  ; j2++) {
@@ -39,7 +30,6 @@ public class TaskFive {
 		System.out.println("Обнавлённая трока:" + str);
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub 
 		TaskFive myTaskFive = new TaskFive();
 		Scanner in = new Scanner(System.in);
 		System.out.println("Введите строку : ");
@@ -48,5 +38,4 @@ public class TaskFive {
 		myTaskFive.convert();
 		myTaskFive.outputNewStr();
 	}
-
 }
