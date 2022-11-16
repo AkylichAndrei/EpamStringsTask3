2580d2586536235cfb9195740432e4c72101f782
Akulich Andrei <103625749+AkylichAndrei@users.noreply.github.com>
2022-11-16 14:47:11 +0300

    Update TestClass.java

--- src/stringsTaskThree/TestClass.java
+++ src/stringsTaskThree/TestClass.java
@@ -1,18 +1,11 @@
 package stringsTaskThree;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 public class TestClass {
-
 	public static void main(String[] args) {
-		// Строка для сканирования, чтобы найти шаблон
 	      String str = "Крещение Руси произошло в 988 году! Не так ли?";
-	      String pattern = "(.*)(\\d+)(.*)";
-
-	      // Создание Pattern объекта
+	      String pattern = "(.*)(\\d+)(.*)";	      
 	      Pattern r = Pattern.compile(pattern);
-
-	      // Создание matcher объекта
 	      Matcher m = r.matcher(str);
 	      if (m.find( )) {
 	         System.out.println("Найдено значение: " + m.group(0));
@@ -22,5 +15,4 @@ public class TestClass {
 	         System.out.println("НЕ СОВПАДАЕТ");
 	      }
 	}
-
 }
